4094b4df7d1dbe88b7c36fdba1aed5d7a1977c6e
YanSido <yansido1@gmail.com>
2021-12-23 12:21:14 +0200

    Added worker form component

--- client/src/core/Main.js
+++ client/src/core/Main.js
@@ -1,5 +1,10 @@
 import React from "react";
+import WorkerForm from "./WorkerForm";
 
 export default function Main() {
-  return <div></div>;
+  return (
+    <div>
+      <WorkerForm />
+    </div>
+  );
 }
---
+++ client/src/core/WorkerForm.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export default function WorkerForm() {
+  return (
+    <Container>
+      <h2>Worker details</h2>
+      <hr />
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">
+            Full Name
+          </label>
+          <input
+            type="name"
+            class="form-control"
+            id="exampleInputName"
+            aria-describedby="NameHelp"
+            placeholder="Enter name"
+            required
+          />
+        </div>
+        <div>
+          <label for="exampleInputPassword1" class="form-label">
+            Work Place
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleInputWorkPlace"
+            placeholder="Enter work place"
+            required
+          />
+        </div>
+        <div class="form-check"></div>
+        <div class="mb-3">
+          <label class="mb-1">Date</label>
+          <br />
+          <input type="date" name="party" min="2017-04-01" max="2022-04-20" required />
+          <span class="validity"></span>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </Container>
+  );
+}

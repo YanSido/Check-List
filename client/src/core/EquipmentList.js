11431848a89e58f409222645d9531ebffb7b572d
YanSido <yansido1@gmail.com>
2021-12-24 23:01:33 +0200

    Added EquipmentList component

---
+++ client/src/core/EquipmentList.js
@@ -0,0 +1,42 @@
+import React, { useState, useRef } from "react";
+import { Container } from "react-bootstrap";
+import { fullEquipmentList } from "../db/equipment";
+
+export default function EquipmentList() {
+  const [missing, setMissing] = useState(0);
+  return (
+    <Container>
+      <table class="table table-hover table-dark text-center">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Item's Name</th>
+            <th scope="col">Full quantity</th>
+            <th scope="col">Current quantity</th>
+            <th scope="col">Missing</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fullEquipmentList.map((equipment, index) => {
+            return (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{equipment.name}</td>
+                <td>{equipment.fullQuantity}</td>
+                <td>
+                  <input
+                    type="text"
+                    id="lquantity"
+                    name="lquantity"
+                    placeholder="Enter current quantity"
+                  />
+                </td>
+                <td>{missing}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Container>
+  );
+}
--- client/src/core/Main.js
+++ client/src/core/Main.js
@@ -1,10 +1,13 @@
 import React from "react";
+import EquipmentList from "./EquipmentList";
 import WorkerForm from "./WorkerForm";
 
 export default function Main() {
   return (
     <div>
       <WorkerForm />
+      <hr />
+      <EquipmentList />
     </div>
   );
 }
